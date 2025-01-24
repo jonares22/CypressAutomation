45cdcbd71a18d152c028d9bd96f238406c33abab
unknown <jonares16@gmail.com>
2025-01-24 22:11:27 +0800

    updated file

--- run_cypress.sh
+++ run_cypress.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+set +e
+
+type=$1
+
 ls
 
 # Install dependencies, uncomment when in pipeline
+echo "****************** Installing node dependencies ******************"
+# npm install
 
-echo "installing node dependencies"
-npm install
+echo "****************** Executing test cases for $type ******************"
+echo "type type $type"
+if [ -z "$type" ]; then
+    echo "No test type selected"
+elif [ "$type" = "ui" ]; then
+    npm run test:ui
+elif [ "$type" = "api" ]; then
+    npm run test:api
+else
+    npm run test:regression
+fi
 
-# Run Cypress tests
-echo "executing test cases"
-npm run test:regression
+npm run allure:generate
